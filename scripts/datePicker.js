38df8de8e5f9c785debe6b725f67991b9ef504f5
alexderrusse98 <you@example.com>
2025-08-13 19:29:24 +0200

    start_page

---
+++ scripts/datePicker.js
@@ -0,0 +1,20 @@
+function init() {
+    datetimer();
+}
+
+function datetimer(){
+    let date = document.getElementById('lblGreetings');
+  const myDate = new Date();
+  const hrs = myDate.getHours();
+
+  let greet;
+
+  if (hrs < 12)
+    greet = 'Good Morning';
+  else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon';
+  else if (hrs >= 17 && hrs <= 24)
+    greet = 'Good Evening';
+        date.innerHTML =
+    '<b>' + greet + '</b> and welcome Alex & Marina & Alex 🙂';
+}
