43e784b5aae0956b166581c26b6f2bc163d6607a
Alex-R298 <alexreitz298@gmail.com>
2025-08-18 13:59:49 +0200

    script.js

--- script.js
+++ script.js
@@ -5,13 +5,39 @@ async function init() {
   fetchen();
 }
 
-const BASE_URL = 'https://join-269aa-default-rtdb.europe-west1.firebasedatabase.app/'; 
+function user_button_show_links() {
+  document.getElementById("myPopup").classList.toggle("show");
+}
+
+function datetimer() {
+  let date = document.getElementById("lblGreetings");
+  if (date) {
+    const myDate = new Date();
+    const hrs = myDate.getHours();
+
+    let greet;
+
+    if (hrs < 12) greet = "Good Morning";
+    else if (hrs >= 12 && hrs <= 17) greet = "Good Afternoon";
+    else if (hrs >= 17 && hrs <= 24) greet = "Good Evening";
+    date.innerHTML = `
+      <h2>${greet}</h2>
+      <p class="greet_name">and welcome Alex & Marina & Alex 🙂</p>
+    `;
+  }
+}
+
+function loadHeader() {
+  const headerContainer = document.getElementById('header-container');
+  if (headerContainer) {
+    headerContainer.innerHTML = getHeaderTemplate();
+  }
+}
 
 
-
-async function fetchen() {
-    let respone = await fetch(`${BASE_URL}.json`);
-    let data = await respone.json();
-    console.log(data);
-    
+function loadSidebar() {
+  const sidebarContainer = document.getElementById('sidebar-container');
+  if (sidebarContainer) {
+    sidebarContainer.innerHTML = getSidebarTemplate();
+  }
 }
